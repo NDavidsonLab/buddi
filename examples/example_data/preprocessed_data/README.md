973d1c522a6677da012732aeb60684fbea1082b4
d33bs <ekgto445@gmail.com>
2026-02-11 00:06:14 +0000

    Deploying to pages from @ NDavidsonLab/buddi_v2@8e2da211fed227a209c78ba1b0e3191dbfe012f2 🚀

--- examples/example_data/preprocessed_data/README.md
+++
@@ -1,1 +0,0 @@
-This is where the output of preprocessing notebooks in the example will be saved
--- src/buddi_v2/plotting/__init__.py
+++
@@ -1,4 +0,0 @@
-from .plot_latent_space import plot_latent_spaces_buddi4
-from .plot_data import plot_data
-
-__all__ = ["plot_latent_spaces_buddi4", "plot_data"]
