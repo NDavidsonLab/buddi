6e1913a153ab569d222be5afd23e7e4aa7d4cadd
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2026-02-10 17:04:32 -0700

    Fixed ruff failure

--- src/buddi_v2/preprocessing/__init__.py
+++ src/buddi_v2/preprocessing/__init__.py
@@ -2,4 +2,4 @@ from . import utils
 from . import sc_augmentor
 from . import generate_pseudo_bulks
 
-__all__ = ['utils', 'sc_augmentor', 'generate_pseudo_bulks']
+__all__ = ["utils", "sc_augmentor", "generate_pseudo_bulks"]
